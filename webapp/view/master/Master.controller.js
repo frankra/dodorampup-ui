32a04815420d92f2ffbe68aa5a89bd36fd37ecfc
Rafael Frank <rafael.frank@sap.com>
2018-06-27 15:29:44 -0300

    Adding databinding and dialog

--- webapp/view/master/Master.controller.js
+++ webapp/view/master/Master.controller.js
@@ -1,9 +1,35 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("com.sap.CloudSCAME.dodorampup.view.master.Master", {
-        onInit: function () { }
+
+        _oUserCreationDialog: null,
+        _oUserDataModel: null,
+        
+        onInit: function () { 
+            this._oUserDataModel = new JSONModel();
+            this.getView().setModel(this._oUserDataModel, "UserModel");
+            this._oUserCreationDialog = this.getView().byId('creationDialog');
+        },
+
+        onNewTaskClicked: function () {
+            this._oUserCreationDialog.open();
+        },
+
+        handleCancelPress: function(){
+            this._oUserCreationDialog.close();
+        },
+
+        handleSavePress: function(){
+            //Persist
+            var oUserData = this._oUserDataModel.getData();
+            
+            console.log(oUserData)
+
+            this._oUserCreationDialog.close();
+        }
     });
 });
